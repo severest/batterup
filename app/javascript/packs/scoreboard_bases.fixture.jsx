9201739403b3d1e8eabc4c2a567a36bd4ebe3864
Sean Everest <sean.b.everest@gmail.com>
2021-05-17 20:56:17 -0700

    eslint

---
+++ app/javascript/packs/scoreboard_bases.fixture.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ScoreboardBases from './scoreboard_bases'
+
+export default <ScoreboardBases />
---
+++ app/javascript/packs/scoreboard_bases.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ScorebaordBases = () => {
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default ScorebaordBases
